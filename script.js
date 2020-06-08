619e7e7b80a48cf92ecb160d6cef56204b892900
Daria Menscikova <menscikova.daria@gmail.com>
2020-06-08 17:11:58 +0200

    show cities from

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const formSearch = document.querySelector('.form-search');
 const inputCitiesFrom = formSearch.querySelector('.input__cities-from');
 const dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from');
@@ -8,6 +7,32 @@ const dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to');
 const city = ['Moscow', 'Amsterdam', 'Chisinau', 'Prague', 'Paris', 'San-Francisco',
 'London', 'Kiev', 'Roma', 'Tel-Aviv'];
 
+const showCity = (input, list) => {
+  list.textContent = '';
+
+  if (input.value !== '') {
+    const filterCity = city.filter((item) => {
+      const fixItem = item.toLowerCase();
+      return fixItem.includes(input.value.toLowerCase());
+    });
+
+    filterCity.forEach((item) => {
+      const li = document.createElement('li');
+      li.classList.add('​dropdown__city');
+      li.textContent = item;
+      list.append(li);
+    });
+  }
+};
+
 inputCitiesFrom.addEventListener('input', () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom);
+});
 
-
+dropdownCitiesFrom.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName.toLowerCase() === 'li') {
+    inputCitiesFrom.value = target.textContent;
+    dropdownCitiesFrom.textContent = '';
+  }
+});
